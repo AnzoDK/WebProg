a7dd4050a8f052e43f969ac5cf6af88a3797563b
AnzoDK <antonr@live.dk>
2022-03-21 13:00:42 +0100

    Started project

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    require_once("sqlConnect.php");
+    $pageID = null;
+    if(isset($_GET['pageID']))
+    {
+        $pageID = $_GET['pageID'];
+    }
+    else
+    {
+        $pageID = 0;
+    }
+    
+    
+    $pageName = GetPageName($pageID);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Spaghetti - Webdev with a death wish
+        </title>
+    </head>
+    <body>
+        <?php
+            echo '<h1 id="pageHeader" class="centered header">' . $pageName .'</h1>'
+        ?>
+    </body>
+    <footer>
+
+    </footer>
+</html>
---
+++ sqlConnect.php
@@ -0,0 +1,16 @@
+<?php
+    $SQL_USERNAME = "root";
+    $SQL_PASSWORD = "localuserpass";
+
+    $DEFAULT_DB = "LocalWeb";
+
+    function GetPageName($id)
+    {
+        
+    }
+
+    function SQL_ExecuteQuery($sql)
+    {
+
+    }
+?>
